d243aa2c6e452827fa91021baec95dfdd6acdad1
Leti <lgsainz15@gmail.com>
2020-12-19 18:55:43 -0500

    created Todo model and connected to postgres db

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+# import psycopg2
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Todo
+db.create_all()
+
 
 @app.route('/')
 def index():
---
+++ models.py
@@ -0,0 +1,7 @@
+from app import db
+# from sqlalchemy.dialects.postgresql import JSON
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    complete = db.Column(db.Boolean)
